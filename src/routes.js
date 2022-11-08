78fdc126a16d4fce3212dfd3d8fe05305cf5568a
sweetNana <dyahnahdya@gmail.com>
2022-11-08 07:49:51 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const { addNoteHandler, getAllNotesHandler } = require('./handler');
+
+const routes = [
+    {
+      method: 'POST',
+      path: '/notes',
+      handler: addNoteHandler,
+    },
+
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+  ];
+   
+  module.exports = routes;
